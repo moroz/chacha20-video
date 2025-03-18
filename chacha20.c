6f2aae847897ced8f32310cf4daeba15aad05d86
Karol Moroz <karol@moroz.dev>
2025-03-18 18:31:33 +0800

    Implement and test QROUND

---
+++ chacha20.c
@@ -0,0 +1,20 @@
+#include "chacha20.h"
+
+static inline uint32_t ROTL32(uint32_t n, int offset) {
+  return (n << offset) | (n >> (32 - offset));
+}
+
+void chacha20_qround(uint32_t *a, uint32_t *b, uint32_t *c, uint32_t *d) {
+  *a += *b;
+  *d ^= *a;
+  *d = ROTL32(*d, 16);
+  *c += *d;
+  *b ^= *c;
+  *b = ROTL32(*b, 12);
+  *a += *b;
+  *d ^= *a;
+  *d = ROTL32(*d, 8);
+  *c += *d;
+  *b ^= *c;
+  *b = ROTL32(*b, 7);
+}
--- chacha20_test.c
+++ chacha20_test.c
@@ -2,7 +2,20 @@
 #include <CUnit/CUnit.h>
 #include <CUnit/TestDB.h>
 
-void test_chacha20_qround(void) {}
+#include "chacha20.h"
+
+void test_chacha20_qround(void) {
+  uint32_t a, b, c, d;
+  a = 0x11111111;
+  b = 0x01020304;
+  c = 0x9b8d6f43;
+  d = 0x01234567;
+  chacha20_qround(&a, &b, &c, &d);
+  CU_ASSERT_EQUAL(a, 0xea2a92f4);
+  CU_ASSERT_EQUAL(b, 0xcb1cf8ce);
+  CU_ASSERT_EQUAL(c, 0x4581472e);
+  CU_ASSERT_EQUAL(d, 0x5881c4bb);
+}
 
 int main(void) {
   CU_initialize_registry();
